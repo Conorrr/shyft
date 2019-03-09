435b7cc1c5be038af43eeb28f8e13a916dff745d
Conor Restall <conor@restall.io>
2019-03-09 20:45:01 +0000

    get webpack wroking

---
+++ static-web/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/main.js',
+    './src/main.css'
+  ],
+
+  output: {
+    filename: 'main-[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    contentBase: 'dist'
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name]-[hash].css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+    new CleanWebpackPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader"
+        ]
+      }
+    ]
+  }
+};
