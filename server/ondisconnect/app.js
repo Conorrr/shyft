65dbce713ad5b08f58907e6a5f5e52eb3ffc742c
Conor Restall <conor@restall.io>
2019-03-12 17:52:57 +0000

    delete connection from dynamodb on disconnect

--- server/ondisconnect/app.js
+++ server/ondisconnect/app.js
@@ -1,13 +1,10 @@
-// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
 var AWS = require("aws-sdk");
 AWS.config.update({ region: process.env.AWS_REGION });
 var DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
 
 exports.handler = function (event, context, callback) {
   var deleteParams = {
-    TableName: process.env.TABLE_NAME,
+    TableName: process.env.CONNECTION_TABLE_NAME,
     Key: {
       connectionId: { S: event.requestContext.connectionId }
     }
